39e512c619020e99683b9943fe1e558a04037ade
Virginie <vb@ikimo9.com>
2025-01-28 21:31:51 +0100

    ajout icones

---
+++ src/components/icons/IconStar.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const IconStar = () => (
+  <svg width="18" height="16" viewBox="0 0 18 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+  <path d="M8.99996 12.8083L14.15 15.9166L12.7833 10.0583L17.3333 6.11665L11.3416 5.60831L8.99996 0.083313L6.65829 5.60831L0.666626 6.11665L5.21663 10.0583L3.84996 15.9166L8.99996 12.8083Z" fill="#627184"/>
+  </svg>
+);
+
+export default IconStar;
